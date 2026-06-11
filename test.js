05abd701cb59a997b55b0ef32f96e73af209cbce
yashkore7 <koreyash009@gmail.com>
2026-06-11 10:20:30 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 console.log("Running application tests...");
 
-const result = 2 + 2;
+const result = 5 + 7;
 
-if (result === 4) {
+if (result === 12) {
   console.log("Tests Passed");
   process.exit(0);
 } else {

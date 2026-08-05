806dea1b809a5f114c34ca6ab4831482e1e8b2a6
yashkore7 <koreyash009@gmail.com>
2026-08-05 09:55:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 8000; 
 app.use(express.json());
 app.get("/", (req, res) => {                 
   res.status(200).json({            
